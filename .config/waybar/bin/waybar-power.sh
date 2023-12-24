0c99540ac8532c15393f19e5144a576440e0a793
Konenass <konenascs@gmail.com>
2023-12-24 05:03:58 +0100

    removed kitty for  alacritty + other things

--- .config/waybar/bin/waybar-power.sh
+++ .config/waybar/bin/waybar-power.sh
@@ -1,2 +1,3 @@
 #!/bin/sh
-swaynag -t custom -m 'Are you sure you want to shutdown your device?' -b 'Poweroff' 'poweroff'
+#swaynag -t custom -m 'Are you sure you want to shutdown your device?' -b 'Poweroff' 'poweroff'
+shutdown now
